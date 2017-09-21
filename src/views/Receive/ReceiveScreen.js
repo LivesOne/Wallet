4303ab475620e0b05c0409b1a4c14e042607e7c6
lijunjie <lijunjieone@gmail.com>
2017-09-21 14:42:00 +0800

    完成简单的界面UI

--- src/views/Receive/ReceiveScreen.js
+++ src/views/Receive/ReceiveScreen.js
@@ -6,20 +6,162 @@
 "use strict";
 
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text,Image } from 'react-native';
+
+
+import LVColor from '../../styles/LVColor';
+import LVStrings from '../../assets/localization';
+
+
+import MXButton from '../../components/MXButton';
+
+const receiveIcon = require("../../assets/images/tab_receive.png");
 
 class ReceiveScreen extends Component {
     static navigationOptions = {
         header: null
     };
+
+
     
     render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+          };
+        
         return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
-                <Text>Receipt Screen</Text>
+            <View style={styles.container}>
+                <View style={styles.topContainer}>
+                    <Text style={styles.title}>
+                        我的收款码
+                    </Text>
+                    <Image source={receiveIcon} style={styles.qrcode_icon}></Image>
+                </View>
+                {/* <View style={styles.mainContainerBackground}> */}
+
+                <View style={styles.mainContainer}>
+
+                <Text style={styles.name} >
+                    钱包地址
+                </Text>
+                <Text style={styles.address}>
+                    abcdefhigjklmopqrst
+                </Text>
+
+                <Image source={pic} style={styles.qrcode_pic}></Image>
+
+                <MXButton
+                    title={"复制地址"}
+                    onPress = {() => {
+                    alert("button clicked");
+                    }}
+                    themeStyle={"active"}
+                /> 
+                <MXButton
+                    title={"保存二维码"}
+                    onPress = {() => {
+                    alert("button clicked");
+                    }}
+                    themeStyle={"active"}
+                />
+                <Image source={receiveIcon} style={styles.share}></Image>
+                </View>
+                {/* </View> */}
             </View>
-        )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: LVColor.white,
+    },
+
+    
+
+    topContainer:{
+        flex:1,
+        flexDirection:'row',
+        alignItems: "center",
+        
+    },
+
+    title:{
+        flex:6,
+        fontSize:18,
+        color:LVColor.grey2,
+        alignItems: "center",
+        textAlign:'center',
+    },
+
+    name:{
+        fontSize:18,
+        color:LVColor.grey2,
+        textAlign:'center',
+    },
+
+    address:{
+        fontSize:15,
+        color:"#c3c8d3",
+    },
+
+    mainContainerBackground:{
+        padding:30,
+        backgroundColor: LVColor.navigationBar,
+        
+    },
+
+    mainContainer:{
+        flex:6,
+        flexDirection:'column',
+        alignItems: 'center',
+        justifyContent:'space-between',
+        backgroundColor: LVColor.navigationBar,
+        padding:40,
+        
+    },
+
+    qrcode_icon: {
+        height:20,
+        width:5,
+        flex:1,
+    },
+
+    qrcode_pic:{
+        height:162,
+        width:162,
+    },
+
+    share:{
+        height:50,
+        width:50,
+    }
+    
+});
+
+const notificationStyles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        marginTop:  20,
+        opacity: 0,
+        width: "100%",
+        height: 0,
+        backgroundColor: "blue"
+    },
+    netLost: {
+        opacity: 1,
+        height: 50
+    },
+    text: {
+        marginLeft: 15,
+        marginRight: 15,
+        fontSize: 14,
+        textAlign: "center",
+        color: "black"
+    }
+});
 
 export default ReceiveScreen;
