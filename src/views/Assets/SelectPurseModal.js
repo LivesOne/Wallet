dba3f24418677fab9356d56c01f661b7989f8de7
XiaoLei Tian <tianxiaolei@maxthon.net>
2017-09-22 21:18:48 +0800

    modal box

---
+++ src/views/Assets/SelectPurseModal.js
@@ -0,0 +1,14 @@
+/*
+ * Project: Venus
+ * File: src/views/Assets/SelectPurseModal.js
+ * @flow
+ */
+'use strict';
+
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Modal from 'react-native-modalbox';
+
+export default class SelectPurseModal extends Component {
+    
+}
