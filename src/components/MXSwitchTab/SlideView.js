d969cc14c858712263906fe07edd04d6a60bd59e
huyinbo <huyinbo@maxthon.net>
2017-09-22 17:13:27 +0800

    add swithTab animation

---
+++ src/components/MXSwitchTab/SlideView.js
@@ -0,0 +1,59 @@
+//@flow
+'use strict'
+
+import React, { Component } from 'react'
+import { View, StyleSheet, Animated } from 'react-native';
+import * as MXUtils from "../../utils/MXUtils";
+
+const SPRING_CONFIG = {tension: 2, friction: 5};
+const SLIDE_VIEW_WIDTH = 30;
+
+export class MXSlideView extends Component {
+
+    state: {
+        pan: any
+    }
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            pan: new Animated.ValueXY()
+        };
+    }
+
+    getStyle() {
+        return [
+                styles.square, 
+                {
+                    transform: this.state.pan.getTranslateTransform()
+                }
+          ];
+    }
+
+    offset(offset: number) {
+        Animated.spring(this.state.pan, {
+            ...SPRING_CONFIG,
+            toValue: {x: offset, y: 0}                       
+      }).start();
+    }
+
+    render() {
+        return (
+            <View style = {[this.props.style, {marginLeft: MXUtils.getDeviceWidth() / 4 - SLIDE_VIEW_WIDTH /2}]}>
+                <Animated.View style={this.getStyle()} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    square: {
+      width: SLIDE_VIEW_WIDTH,
+      height: 2,
+      backgroundColor: 'grey'
+    } 
+  });
+
+export default MXSlideView;
