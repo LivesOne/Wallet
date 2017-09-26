3fdf5be1171b7792307d5d235d426b40d9d249ee
huyinbo <huyinbo@maxthon.net>
2017-09-26 21:42:09 +0800

    fix android crypto bug

--- android/app/src/main/java/com/venus/CryptoUtils.java
+++ android/app/src/main/java/com/venus/CryptoUtils.java
@@ -1,5 +1,6 @@
 package com.venus;
 
+
 /**
  * Created by tlrkboy on 26/09/2017.
  */
@@ -13,11 +14,9 @@ public class CryptoUtils {
         System.loadLibrary("scrypt_crypho");
     }
 
-    public static native byte[] scrypt(byte[] pass, char[] salt, Integer N, Integer r, Integer p, Integer dkLen);
-
-    public static String scrypt(String password, String salt, int N, int r, int p, int dkLen) {
+    public static String scrypt(String password, String saltHexStr, int N, int r, int p, int dkLen) {
         byte[] pass = password != null ? password.getBytes() : null;
-        char[] s = salt != null ? salt.toCharArray() : null;
+        char[] s = saltHexStr != null ? toHex(saltHexStr) : null;
         if (pass == null || s == null) {
             return null;
         } else {
@@ -27,6 +26,19 @@ public class CryptoUtils {
                 return null;
             }
         }
+    }
+
+    private static native byte[] scrypt(byte[] pass, char[] salt, Integer N, Integer r, Integer p, Integer dkLen);
+
+    public static char[] toHex(String input) {
+        if(input != null && input.length() % 2 == 0) {
+            char[] data = new char[input.length()/2];
+            for (int i = 0; i < input.length(); i+=2) {
+                data[i/2] = (char) Integer.parseInt(input.substring(i, i+2), 16);
+            }
+            return data;
+        }
+        return null;
     }
 
     private static String hexify (byte[] input) {
--- android/app/src/main/java/com/venus/MainActivity.java
+++ android/app/src/main/java/com/venus/MainActivity.java
@@ -1,6 +1,5 @@
 package com.venus;
 
-import android.util.Log;
 
 import com.facebook.react.ReactActivity;
 
@@ -15,46 +14,4 @@ public class MainActivity extends ReactActivity {
         return "Venus";
     }
 
-//    lvExport.libscrypt("showmethemoney",
-//            "8d0d63de8dc0a0b7e8c6dba44c7dd4750f5df49a4d040d4458f3b23e579722af"
-//            ,262144,8,1);
-
-//        "salt": "salt",
-//                "password": "password",
-//                "N": 2,
-//                "r": 1,
-//                "p": 1,
-//                "dkLen": 32,
-//                "expected": "6d1bb878eee9ce4a7b77d7a44103574d4cbfe3c15ae3940f0ffe75cd5e1e0afa"
-
-//    {
-//        "salt": "NaCl", // heh
-//            "password": "password",
-//            "N": 1024,
-//            "r": 8,
-//            "p": 16,
-//            "dkLen": 64,
-//            "expected": "fdbabe1c9d3472007856e7190d01e9fe7c6ad7cbc8237830e77376634b3731622eaf30d92e22a3886ff109279d9830dac727afb94a83ee6d8360cbdfa2cc0640"
-//    },
-
-//    {
-//        "salt": "SodiumChloride",
-//            "password": "pleaseletmein",
-//            "N": 16384,
-//            "r": 8,
-//            "p": 1,
-//            "dkLen": 64,
-//            "expected": "7023bdcb3afd7348461c06cd81fd38ebfda8fbba904f8e3ea9b543f6545da1f2d5432955613f0fcf62d49705242a9af9e61e85dc0d651e40dfcf017b45575887"
-//    },
-    @Override
-    protected void onResume() {
-        super.onResume();
-        String res = CryptoUtils.scrypt("pleaseletmein", "SodiumChloride",
-                16384, 8, 1, 64);
-        if (res != null && "7023bdcb3afd7348461c06cd81fd38ebfda8fbba904f8e3ea9b543f6545da1f2d5432955613f0fcf62d49705242a9af9e61e85dc0d651e40dfcf017b45575887".equals(new String(res))) {
-            Log.i("Venus", "crypto ok reuslt = " + new String(res));
-        } else {
-            Log.i("Venus", "crypto failed, result = " + new String(res));
-        }
-    }
 }
