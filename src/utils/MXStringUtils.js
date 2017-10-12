8ad98ec153de8048e58805d27e1122b9982f9d3c
XiaoLei Tian <tianxiaolei@maxthon.net>
2017-10-12 15:07:26 +0800

    modify string utils

--- src/utils/MXStringUtils.js
+++ src/utils/MXStringUtils.js
@@ -2,25 +2,27 @@
 
 /**
 * Convert the number like 20000 into '20,000' this form of the amount of the string.
-* The fractional part of the number will be retained, eg: 2000.050 -> '2,000.050'
+* The fraction digits will be retained if precision is not set, eg: 2000.050 -> '2,000.050'
 */
-export function convertAmountToCurrencyString(amount: number, thousandsSeparator: string): string {
+export function convertAmountToCurrencyString(amount: number, thousandsSeparator: ?string, precision: ?number): string {
     const sep = thousandsSeparator || ',';
-    const arr = (amount + '').split('.');
+    const arr = (precision ? amount.toFixed(precision) : (amount + '')).split('.');
 
-    let num = (arr[0] || 0).toString();
     let result = '';
+    let num = (arr[0] || 0).toString();
+    let decimal = (arr[1] || 0).toString();
 
     while (num.length > 3) {
         result = sep + num.slice(-3) + result;
         num = num.slice(0, num.length - 3);
     }
-    if (num) {
-        result = num + result;
-        if (arr.length === 2) {
-            result = result + '.' + arr[1];
-        }
+
+    result = num + result;
+
+    if (arr.length === 2) {
+        result = result + '.' + decimal;
     }
+
     return result;
 }
 
@@ -31,16 +33,13 @@ export function convertAmountToCurrencyString(amount: number, thousandsSeparator
  * @param  {number=3} tailNum
  * @returns string
  */
-export function converAddressToDisplayableText(address: string, headNum: number = 3, tailNum: number = 3) : string {
-    if(headNum + tailNum + 3 >= address.length
-        || headNum === 0 
-        || headNum + 3 >= address.length
-        || tailNum === 0) {
+export function converAddressToDisplayableText(address: string, headNum: number = 3, tailNum: number = 3): string {
+    if (headNum + tailNum + 3 >= address.length || headNum === 0 || headNum + 3 >= address.length || tailNum === 0) {
         return address;
     }
 
     const upaddr = address.toUpperCase();
-    const head = upaddr.substr(0,headNum);
+    const head = upaddr.substr(0, headNum);
     const tail = upaddr.substr(upaddr.length - tailNum - 1, tailNum);
-    return ['0x',head,'...',tail].join('');
+    return ['0x', head, '...', tail].join('');
 }
