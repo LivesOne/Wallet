2be9adb329479e635e3757cde3f6e59591426d97
lijunjie <lijunjieone@gmail.com>
2017-10-17 16:34:45 +0800

    修改收款ui,避免收款title滑动

--- src/views/Receive/ReceiveScreen.js
+++ src/views/Receive/ReceiveScreen.js
@@ -165,11 +165,12 @@ class ReceiveScreen extends Component {
         if(walletIsBlank) {
 
          return (
-            <ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor:LVColor.white }}  contentContainerStyle={styles.contentContainer}>
             
             <View style={styles.container}>
                 <ReceiveHeader callback={this.onPressSelectWallet}/>
 
+                <View style={styles.mainContainer2}>
+                <ScrollView showsVerticalScrollIndicator={false} style={{ width:'100%', }}  contentContainerStyle={styles.contentContainer}>
                 <View style={styles.mainContainer}>
 
                 <Text style={styles.name} >
@@ -232,6 +233,8 @@ class ReceiveScreen extends Component {
                     }
                    ></MxImage>
                 </View>
+                </ScrollView>
+                </View>
 
                 <LVSelectWalletModal
                     isOpen={this.state.openSelectWallet}
@@ -241,7 +244,6 @@ class ReceiveScreen extends Component {
                 />
 
             </View>
-            </ScrollView>
         );
     }else {
         return (
@@ -277,9 +279,11 @@ const styles = StyleSheet.create({
         
     },
     contentContainer: {
-        paddingVertical: 20,
-        backgroundColor: LVColor.white,
+        // paddingVertical: 20,
+        // backgroundColor: 'blue',
         // height:'100%',
+        alignItems: 'center',
+        justifyContent:'center',
     },
 
     topContainer:{
@@ -287,7 +291,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         alignItems: "center",
         paddingTop:10,
-        paddingBottom:20,
+        paddingBottom:10,
         justifyContent:'center',
     },
 
@@ -333,6 +337,8 @@ const styles = StyleSheet.create({
         width:'90%',
         flexDirection:'column',
         alignItems: 'center',
+        justifyContent:'center',
+        
         backgroundColor: LVColor.navigationBar,
         // backgroundColor:'black',
         elevation: 20,
@@ -344,7 +350,8 @@ const styles = StyleSheet.create({
      },
 
      mainContainer2:{
-         flex:6,
+         flex:7,
+         width:'100%',
          flexDirection:'column',
          alignItems: 'center', 
         //  backgroundColor:'red',
