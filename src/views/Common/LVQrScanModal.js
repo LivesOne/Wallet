ebe5984ca22b6273784202f92b3cf6c9a5ce4284
tlrkboy <huyinbo@maxthon.net>
2018-06-04 14:39:27 +0800

    解决ios二维码扫描没有授权的情况下界面异常

--- src/views/Common/LVQrScanModal.js
+++ src/views/Common/LVQrScanModal.js
@@ -68,9 +68,11 @@ export class LVQrScanModal extends React.Component<Props, State> {
                     notAuthorizedView={
                         <View
                             style={{
+                                position: 'absolute',
+                                width: '100%',
+                                height:'100%',
                                 flex: 1,
                                 alignItems: 'center',
-                                justifyContent: 'center'
                             }}
                         >
                             <Text style={{ textAlign: 'center', fontSize: 16 }}>
