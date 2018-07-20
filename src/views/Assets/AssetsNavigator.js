7f355340af3933b3ade185760a4ecd12edcacad1
XiaoLei Tian <tianxiaolei@maxthon.net>
2018-07-20 14:23:28 +0800

    modify wallet header

--- src/views/Assets/AssetsNavigator.js
+++ src/views/Assets/AssetsNavigator.js
@@ -70,7 +70,31 @@ const AssetsNavigator = StackNavigator(
     },
     {
         mode: 'modal',
-        headerMode: 'none'
+        headerMode: 'none',
+        transitionConfig: () => ({
+            transitionSpec: {
+              duration: 300,
+              easing: Easing.out(Easing.poly(4)),
+              timing: Animated.timing,
+            },
+            screenInterpolator: sceneProps => {
+              const { layout, position, scene } = sceneProps;
+              const { index } = scene;
+      
+              const height = layout.initHeight;
+              const translateY = position.interpolate({
+                inputRange: [index - 1, index, index + 1],
+                outputRange: [height, 0, 0],
+              });
+      
+              const opacity = position.interpolate({
+                inputRange: [index - 1, index - 0.99, index],
+                outputRange: [0, 1, 1],
+              });
+      
+              return { opacity, transform: [{ translateY }] };
+            }
+          }),
     }
 );
 
